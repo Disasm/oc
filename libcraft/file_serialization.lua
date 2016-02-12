34bdd912a1c9f8c7d6088a904935107cbbbeced4
Riateche <ri@idzaaus.org>
2016-02-13 00:33:53 +0300

    update syntax

--- libcraft/file_serialization.lua
+++ libcraft/file_serialization.lua
@@ -4,17 +4,17 @@ local filesystem = require("filesystem")
 
 local file_serialization = {}
 
-file_serialization.load = function(filename) {
+file_serialization.load = function(filename) 
   f = filesystem.open(filename, "r")
   local s = f:read(10000)
   f:close()
   return serialization.unserialize(s)
-}
+end
 
-file_serialization.save = function(filename, object) {
+file_serialization.save = function(filename, object) 
   local f = filesystem.open(filename, "w")
   f:write(serialization.serialize(recipe))
   f:close()
-}
+end
 
 return file_serialization
