0306bb370ed4bb0b0f3d333b829b5d13b5e9b689
Riateche <ri@idzaaus.org>
2016-02-13 00:25:21 +0300

    update updater

--- update_scripts.lua
+++ update_scripts.lua
@@ -1,6 +1,6 @@
 local shell = require("shell")
 url = "https://raw.githubusercontent.com/Disasm/oc/master/"
-files = { "base64", "g", "goto", "libs/movement", "tar", "update_scripts", "craft", "libcraft/craft", "libcraft/craft_chests", "libcraft/craft_db", "libcraft/craft_input" }
+files = { "g", "goto", "libs/movement", "update_scripts", "craft", "libcraft/craft", "libcraft/craft_chests", "libcraft/craft_db", "libcraft/craft_input" }
 for i = 1, #files do 
   local name = files[i]..".lua"
   shell.execute("rm \""..name.."\"")
