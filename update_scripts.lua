52dc06dcf518a9b185626b02925dc8a6ca98ba09
Disasm <admin@disasm.info>
2016-02-26 23:22:11 +0300

    updater improved

--- update_scripts.lua
+++ update_scripts.lua
@@ -1,5 +1,15 @@
 local shell = require("shell")
-url = "http://www.idzaaus.org/static/tmp/oc/"
+local event = require("event")
+
+print("click on screen")
+local ev = table.pack(event.pull("touch"))
+local url
+if ev[6] == "Riateche" then
+  url = "http://www.idzaaus.org/static/tmp/oc/"
+else
+  url = "http://42b.ru/oc/tree/"
+end
+
 files = { "g", "goto", "libs/movement", "update_scripts", "craft", "libcraft/craft", "libcraft/craft_chests", "libcraft/craft_db", "libcraft/craft_input", "libcraft/file_serialization" }
 for i = 1, #files do 
   local name = files[i]..".lua"
