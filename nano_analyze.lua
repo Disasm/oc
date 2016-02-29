fe94c5af46bc5369109bb092cc827bfb84f7ecf8
Disasm <admin@disasm.info>
2016-02-29 22:35:03 +0300

    nanomachine tools

---
+++ nano_analyze.lua
@@ -0,0 +1,101 @@
+local file_serialization = require("file_serialization")
+local serialization = require("serialization")
+local filesystem = require("filesystem")
+local io = require("io")
+local component = require("component")
+local gpu = component.gpu 
+local event = require("event")
+local keyboard = require("keyboard")
+
+local bad_things = {};
+for i, name in ipairs({"poison", "confusion", "weakness", "moveSlowdown", "digSlowDown", "wither", "blindness", "hunger"}) do 
+  bad_things[name] = true
+end 
+
+
+function waitForEnter()
+    while true do
+        local e, addr, ch, code, player = event.pull("key_down")
+        if code == keyboard.keys.enter then
+            break
+        end
+    end
+end
+
+print("click on screen")
+local ev = table.pack(event.pull("touch"))
+player_name = ev[6]
+directory = "nanodata/"..player_name
+-- all_data = {}
+effects = {}
+
+for fileName in filesystem.list(directory) do
+  local data = file_serialization.load(directory.."/"..fileName)
+  local good_data = {}
+  local good_data_any = false
+  local any_positive = false 
+  for i, value in ipairs(data.effects) do
+    if not string.find(value, "particles.") then 
+      good_data[#good_data + 1] = value 
+      good_data_any = true 
+      if not bad_things[value] then 
+        any_positive = true
+      end
+    end 
+  end
+  if good_data_any and any_positive then 
+    effects_string = serialization.serialize(good_data)
+    
+    if not effects[effects_string] then 
+      effects[effects_string] = data.inputs
+    else 
+      if #(data.inputs) < #(effects[effects_string]) then 
+        effects[effects_string] = data.inputs
+      end 
+    end 
+  end 
+end
+
+local function effects_rating(effects)
+  local r = 0 
+  for i, effect in ipairs(effects) do 
+    if bad_things[effect] then 
+      r = r - 3
+    else 
+      r = r + 1
+    end 
+  end 
+  return r 
+end
+
+effects_list = {}
+for effs, inputs in pairs(effects) do 
+  v = {}
+  v.effects = serialization.unserialize(effs)
+  v.inputs = inputs 
+  effects_list[#effects_list + 1] = v 
+end 
+
+table.sort(effects_list, function(a,b) return effects_rating(a.effects) > effects_rating(b.effects) end)
+
+
+local linesPrinted = 0 
+for i, v in ipairs(effects_list) do 
+  io.write(serialization.serialize(v.inputs).." => ")
+  for i, effect in ipairs(v.effects) do 
+    if bad_things[effect] then 
+      gpu.setForeground(0xff0000)
+    else 
+      gpu.setForeground(0x00ff00)    
+    end 
+    io.write(effect .. " ")
+    gpu.setForeground(0xffffff)
+  end 
+  io.write("\n")
+  linesPrinted = linesPrinted + 1
+  if linesPrinted > 20 then 
+    waitForEnter()
+    linesPrinted = 0
+  end 
+  
+end
---
+++ nano_scan.lua
@@ -0,0 +1,181 @@
+local nanomachine = require("nanomachine")
+local m = require("component").modem
+local event = require("event")
+local ser = require("serialization")
+local fs = require("filesystem")
+local unicode = require("unicode")
+local file_serialization = require("file_serialization")
+
+local machine = nanomachine.find()
+if machine == nil then
+  print("nanomachine is not found")
+  return
+end
+
+local input_count = 15
+
+local function splitComma(str)
+  str = str:sub(2, -2)
+  local l = {}
+  for i in str:gmatch("(.-),.-") do
+    table.insert(l, i)
+  end
+  table.insert(l, str:match(".+,(.+)"))
+  if #l == 0 then
+    if str ~= "" then
+      table.insert(l, str)
+    end
+  end
+  return l
+end
+
+-- init
+input_count = machine.getTotalInputCount()
+local player_name = machine.getName()
+print("Player name is " .. player_name)
+print("TotalInputCount: " .. input_count)
+print("SafeActiveInputs: " .. machine.getSafeActiveInputs())
+print("MaxActiveInputs: " .. machine.getMaxActiveInputs())
+
+local function clear()
+  print("Turning off inputs")
+  for i = 1, input_count, 1 do
+    machine.setInput(i, false)
+  end
+end
+
+local function getState()
+  for i = 1, input_count, 1 do
+    print("Input "..i.." is "..tostring(machine.getInput(i)));
+  end 
+end 
+
+local function stateToString(state) 
+  local results = {}
+  for i = 1, input_count do 
+    if state[i] then 
+      results[#results + 1] = tostring(i)
+    end
+  end
+  local result = table.concat(results, "_")
+  if result == "" then 
+    result = "0"
+  end 
+  return result
+end
+
+clear();
+print("Scanning")
+local current_state = {}
+for i = 1, input_count do 
+  current_state[i] = false
+end
+
+local function setState(state) 
+  for i = 1, input_count do 
+    if state[i] == false and current_state[i] == true then 
+      --print("Setting input "..tostring(i).." to "..tostring(state[i]))
+      machine.setInput(i, state[i])
+      current_state[i] = state[i]
+    end
+  end
+  for i = 1, input_count do 
+    if state[i] == true and current_state[i] == false then 
+      --print("Setting input "..tostring(i).." to "..tostring(state[i]))
+      machine.setInput(i, state[i])
+      current_state[i] = state[i]
+    end
+  end
+end
+
+local function fastClear()
+  local state = {}
+  for i = 1, input_count do 
+    state[i] = false
+  end
+  setState(state)
+end 
+
+local function effectsFileName(state)
+  return "nanodata/"..player_name.."/"..stateToString(state)..".txt"
+end
+
+local function saveActiveEffects()
+  local effects = splitComma(machine.getActiveEffects()) 
+  local data = {}
+  data.effects = effects 
+  local inputs = {}
+  for i = 1, input_count do 
+    if current_state[i] then 
+      inputs[#inputs + 1] = i
+    end 
+  end 
+  data.inputs = inputs 
+  file_serialization.save(effectsFileName(current_state), data)
+  print(ser.serialize(inputs).." => "..ser.serialize(effects))  
+end
+
+
+
+
+-- local healthCheckCoolDown = 0;
+local function checkHealth()
+  -- if healthCheckCoolDown == 0 then 
+    -- healthCheckCoolDown = 5
+  local v = machine.getHealth() 
+  -- print("Health: "..tostring(v))
+  if v < 7 then 
+    fastClear()
+    error("Your health is dangerously low!")
+  end 
+  -- else 
+   --  healthCheckCoolDown = healthCheckCoolDown - 1 
+  -- end 
+end 
+
+
+
+local function testState(state)
+  if not file_serialization.load(effectsFileName(state)) then 
+    checkHealth()
+    setState(state)
+    saveActiveEffects()
+  end
+end
+
+for i = 1, input_count do 
+  local state = {}
+  for j = 1, input_count do 
+    state[j] = (i == j)
+  end
+  testState(state)
+end 
+
+for i = 1, input_count do 
+  for j = (i + 1), input_count do 
+    local state = {}
+    for s = 1, input_count do 
+      state[s] = (s == i or s == j)
+    end
+    testState(state)
+  end
+end 
+
+for i = 1, input_count do 
+  for j = (i + 1), input_count do 
+    for z = (j + 1), input_count do 
+      local state = {}
+      for s = 1, input_count do 
+        state[s] = (s == i or s == j or s == z)
+      end
+      testState(state)
+    end
+  end
+end 
+
+
+
+fastClear()
+
+-- getState();
+
