c89e6314f4690627b2950c6df5d135a1c9cc7014
Disasm <admin@disasm.info>
2016-03-05 02:37:33 +0300

    improve trade storage

--- trade_storage.lua
+++ trade_storage.lua
@@ -4,14 +4,16 @@ local sides = require("sides")
 local util = require("stack_util")
 
 local outputSide = sides.up
-local outputSlot = 16
-local storageSides = {sides.left, sides.right, sides.front, sides.back}
+local outputSlot = 16+4
+local storageSides = {sides.left--[[, sides.right, sides.front, sides.back]]--
+}
 
 local storageCache = {}
 
 storage = {}
 
 function storage.scan()
+  storageCache = {}
   for _,side in pairs(storageSides) do
     local cache = {}
 
@@ -37,13 +39,29 @@ function storage.getStackSize(stack)
   return cnt
 end
 
+function storage.getOutputInventorySize()
+  return 16
+end
+
+function storage.getStackInOutputSlot(slot)
+  return transposer.getStackInSlot(outputSide, slot + 4)
+end
+
 function storage.moveToOutput(stack)
+  local s = transposer.getStackInSlot(outputSide, outputSlot)
+  if (s ~= nil) and (not util.equalThings(s, stack)) then
+    return false
+  end
+  if (transposer.getSlotMaxStackSize(outputSide, outputSlot) - s.size) < stack.size then
+    return false
+  end
+
   local cnt = stack.size
   for side,cache in pairs(storageCache) do
     for slot,s in pairs(cache) do
       if util.equalThings(s, stack) then
         local take = math.min(s.size, cnt)
-        transposer.transferItem(side, outputSide, take, slot)
+        transposer.transferItem(side, outputSide, take, slot, outputSlot)
         cnt = cnt - take
         cache[slot] = transposer.getStackInSlot(side, slot)
       end
@@ -53,10 +71,16 @@ function storage.moveToOutput(stack)
 end
 
 function storage.moveToStorage(slot)
-  local stack = transposer.getStackInSlot(outputSide, slot)
+  if storage.getFreeSlotCount() < 1 then
+    -- not enough space
+    return false
+  end
+  local stack = storage.getStackInOutputSlot(slot)
   if stack == nil then
     error("there is no stack in slot "..tostring(slot), 2)
   end
+  slot = slot + 4 -- robot has some internal slots too
+
   local cnt = stack.size
   for side,cache in pairs(storageCache) do
     for slot2,s in pairs(cache) do
@@ -65,7 +89,7 @@ function storage.moveToStorage(slot)
         local take = math.min(freeSpace, cnt)
         transposer.transferItem(outputSide, side, take, slot, slot2)
         cnt = cnt - take
-        cache[slot] = transposer.getStackInSlot(side, slot2)
+        cache[slot2] = transposer.getStackInSlot(side, slot2)
       end
     end
   end
@@ -75,7 +99,7 @@ function storage.moveToStorage(slot)
       local s = cache[slot2]
       if s == nil then
         transposer.transferItem(outputSide, side, stack.size, slot, slot2)
-        cache[slot] = transposer.getStackInSlot(side, slot2)
+        cache[slot2] = transposer.getStackInSlot(side, slot2)
         return true
       end
     end
@@ -84,10 +108,13 @@ function storage.moveToStorage(slot)
 end
 
 function storage.moveAllToStorage()
-  --local n = transposer.getInventorySize(outputSide)
-  --for slot=1,n do
-  for slot=5,20 do
-    local stack = transposer.getStackInSlot(outputSide, slot)
+  local n = storage.getOutputInventorySize()
+  if storage.getFreeSlotCount() < n then
+    -- not enough space
+    return false
+  end
+  for slot=1,n do
+    local stack = storage.getStackInOutputSlot(slot)
     if stack ~= nil then
       local ok = storage.moveToStorage(slot)
       if not ok then
@@ -96,6 +123,29 @@ function storage.moveAllToStorage()
     end
   end
   return true
+end
+
+function storage.getFreeSlotCount()
+  local cnt = 0
+  for side,cache in pairs(storageCache) do
+    local n = transposer.getInventorySize(side)
+    for slot=1,n do
+      if cache[slot] == nil then
+        cnt = cnt + 1
+      end
+    end
+  end
+  return cnt
+end
+
+function storage.dump()
+  for side,cache in pairs(storageCache) do
+    for slot,s in pairs(cache) do
+      if s ~= nil then
+        print(sides[side].." "..slot.." "..s.label.." x "..s.size)
+      end
+    end
+  end
 end
 
 return storage
