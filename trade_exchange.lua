e7acc5845d56ae076d323a312b2fab8fde322c6e
Vadim Kaushan <admin@disasm.info>
2016-03-08 15:49:59 +0300

    improvements and fixes for exchange code

--- trade_exchange.lua
+++ trade_exchange.lua
@@ -42,8 +42,31 @@ function ex:addLot(username, fromStack, toStack, count)
   self:save()
 end
 
-function ex:getAllLots()
-  return self.lots
+function ex:getAllLots(real_counts)
+  real_counts = not not real_counts
+  r = {}
+  for id,lot in pairs(self.lots) do
+    if type(id) == "number" then
+      if real_counts then
+        local real_count = self:getRealExchangeCount(id)
+        if real_count > 0 then
+          local lot_copy = {}
+          for k,v in pairs(lot) do
+            lot_copy[k] = v
+          end
+          lot_copy.count = real_count
+          r[#r+1] = lot_copy
+        end
+      else
+        local lot_copy = {}
+        for k,v in pairs(lot) do
+          lot_copy[k] = v
+        end
+        r[#r+1] = lot_copy
+      end
+    end
+  end
+  return r
 end
 
 function ex:exchange(lotId, username2, count)
@@ -73,6 +96,27 @@ function ex:exchange(lotId, username2, count)
   end
 end
 
+
+-- Exchange count that owner can exchange given his items and free space
+function ex:getRealExchangeCount(lotId)
+  local lot = self.lots[lotId]
+  if lot == nil then
+    return 0
+  end
+
+  local fromStack = makeStack(lot.from)
+  local toStack = makeStack(lot.to)
+
+  local cnt1 = item_db:getStackSize(lot.username, fromStack)
+  local free1 = item_db:getFreeSpaceForStack(lot.username, toStack)
+
+  local n1 = math.floor(cnt1 / fromStack.size)
+  local n2 = math.floor(free1 / toStack.size)
+  return math.min(n1, n2)
+end
+
+
+-- Exchange count that other user can exchange given his and owner's items and free space
 function ex:getMaxExchangeCount(lotId, username2)
   local lot = self.lots[lotId]
   if lot == nil then
@@ -85,15 +129,15 @@ function ex:getMaxExchangeCount(lotId, username2)
   local fromStack = makeStack(lot.from)
   local toStack = makeStack(lot.to)
 
-  local cnt1 = item_db:getStackSize(username2, fromStack)
-  local cnt2 = item_db:getStackSize(lot.username, toStack)
-  local free1 = item_db:getFreeSpaceForStack(username2, toStack)
-  local free2 = item_db:getFreeSpaceForStack(lot.username, fromStack)
+  local cnt1 = item_db:getStackSize(username2, toStack)
+  local cnt2 = item_db:getStackSize(lot.username, fromStack)
+  local free1 = item_db:getFreeSpaceForStack(username2, fromStack)
+  local free2 = item_db:getFreeSpaceForStack(lot.username, toStack)
 
-  local n1 = math.floor(cnt1 / fromStack.size)
-  local n2 = math.floor(cnt2 / toStack.size)
-  local n3 = math.floor(free1 / toStack.size)
-  local n4 = math.floor(free2 / fromStack.size)
+  local n1 = math.floor(cnt1 / toStack.size)
+  local n2 = math.floor(cnt2 / fromStack.size)
+  local n3 = math.floor(free1 / fromStack.size)
+  local n4 = math.floor(free2 / toStack.size)
   return math.min(math.min(n1, n2), math.min(n3, n4))
 end
 
