7e3c7fe74a2b7ad9074b10cfae65319428708876
Riateche <ri@idzaaus.org>
2016-03-22 22:25:18 +0300

    old change

--- shop_guest_watcher.lua
+++ shop_guest_watcher.lua
@@ -1,9 +1,17 @@
 local event = require("event")
 local computer = require("computer")
+local real_time = require("real_time")
+local filesystem = require("filesystem")
 
 function on_motion(e, addr, x, y, z, name)
-  print("Detected: " .. name)
+  if name == "Riateche" or name == "disasm" then 
+    return 
+  end
+  local f = filesystem.open("/visits.log", "a")
+  if f then
+    f:write(string.format("[%s] Detected: %s\n", real_time.get_string(), name))
+    f:close()
+  end
 end
-
 
 event.listen("motion", on_motion)
