e366893c4b61ae6612cc5fa8f6d22947b202ec4f
Riateche <ri@idzaaus.org>
2016-03-22 22:25:02 +0300

    file serialization can create folders and reports errors

--- libs/file_serialization.lua
+++ libs/file_serialization.lua
@@ -1,11 +1,17 @@
 local serialization = require("serialization")
 local filesystem = require("filesystem")
-
+local shell = require("shell")
 
 local file_serialization = {}
 
+function strip_filename(filename)
+  pos = string.find(string.reverse(filename), "/")
+  if pos == nil then return nil end 
+  return string.sub(filename, 0, string.len(filename) - pos)
+end
+
 file_serialization.load = function(filename)
-  f = filesystem.open(filename, "r")
+  local f = filesystem.open(filename, "r")
   if f == nil then return nil end
   local s = ""
   buf = f:read(2048)
@@ -19,6 +25,16 @@ end
 
 file_serialization.save = function(filename, object, add_return)
   local f = filesystem.open(filename, "w")
+  if not f then 
+    local path = strip_filename(filename)
+    if path then 
+      shell.execute(string.format("mkdir -p %s", path))
+      f = filesystem.open(filename, "w")
+    end
+  end 
+  if not f then 
+    error("Failed to write to "..filename)
+  end  
   local s = serialization.serialize(object)
   if add_return then 
     s = "return "..s
