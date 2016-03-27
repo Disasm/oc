1dbcd5c113c4c9746f0ed7f72b8cd7fc2094da70
Riateche <ri@idzaaus.org>
2016-03-27 23:06:06 +0300

    master api changed

--- craft2/item_storage.lua
+++ craft2/item_storage.lua
@@ -1,129 +1,134 @@
 
-local module_r = {} 
+local module_r = {}
 function module_r.create_storage()
   local master = require("craft2/master_main")
-  local l = master.log 
+  local l = master.log
   local r = {}
-  
+
   local item_db = require("craft2/item_database")
   local function max_stack(item_id)
     return item_db.get(item_id).maxStackSize
   end
-  
+
   local function score_for_adding_items(stack, source_chest, sink_chest)
     local distance = source_chest.distances_to_chests[sink_chest.id]
     local free_slots = sink_chest.free_slots_count()
     local existing_space = sink_chest.space_in_existing_slots(stack[2])
-    if existing_space > stack[1] then 
+    if existing_space > stack[1] then
       existing_space = stack[1]
-      free_slots = free_slots + 1 
-    end 
-    if free_slots == 0 then 
+      free_slots = free_slots + 1
+    end
+    if free_slots == 0 then
       return -1000
-    end 
+    end
     local score = 1000.0
-    if free_slots < 3 then 
+    if free_slots < 3 then
       score = score - 10
-    else 
-      score = score + free_slots * 0.1 
-    end 
+    else
+      score = score + free_slots * 0.1
+    end
     score = score - distance
-    if existing_space > 0 then 
+    if existing_space > 0 then
       score = score + 15
-    end 
+    end
     l.dbg(string.format("chest=%d score=%d (dist=%d slots=%d e.s.=%d)", sink_chest.id, score, distance, free_slots, existing_space))
-    return score 
-  end 
-    
-  
-  function r.load_all_from_chest(source_chest, task) 
-    for i = 1, source_chest.slots_count do 
+    return score
+  end
+
+
+  function r.load_all_from_chest(source_chest, task)
+    for i = 1, source_chest.slots_count do
       l.dbg("Processing slot "..i)
-      if i ~= 2 then 
+      if i ~= 2 then
         l.dbg("test1")
         local stack = source_chest.get_istack(i)
-        l.dbg("test2 "..l.inspect(stack))        
-        if stack[1] > 0 then 
+        l.dbg("test2 "..l.inspect(stack))
+        if stack[1] > 0 then
           l.dbg("stack[1] > 0")
           local max_score = -1
           local max_score_chest = nil
-          for j, sink_chest in ipairs(master.chests) do 
+          for j, sink_chest in ipairs(master.chests) do
             l.dbg("iterating: chest "..j)
-            if sink_chest.role == "storage" then 
+            if sink_chest.role == "storage" then
               l.dbg("Calling score_for_adding_items")
               local score = score_for_adding_items(stack, source_chest, sink_chest)
-              if score > max_score then 
-                max_score = score 
+              if score > max_score then
+                max_score = score
                 max_score_chest = sink_chest
-              end 
-            end 
-          end 
-          if not max_score_chest then 
+              end
+            end
+          end
+          if not max_score_chest then
             task.status = "error"
             task.status_message = "No available chests to store items!"
             l.dbg("fail 1")
-            return false 
-          end 
+            return false
+          end
           l.dbg(string.format("transfer from slot %d into chest %d", i, max_score_chest.id))
-          if not source_chest.transfer_to(max_score_chest, stack[1], i, nil) then 
+          if not source_chest.transfer_to(max_score_chest, stack[1], i, nil) then
             task.status = "error"
             task.status_message = "Transfer failed."
             l.dbg("fail 2")
-            return false 
-          end 
-        end 
-      end
-    end 
-    l.info("Loading items from chest succeeded.")
-    return true  
-  end 
-  
-  function r.get_total_count(item_id)
-    local result = 0
-    for _, chest in ipairs(master.chests) do 
-      for i = 3, chest.slots_count do 
-        local stack = chest.get_istack(i)
-        if item_id == stack[2] then 
-          result = result + stack[1]
-        end 
-      end
-    end 
-    return result 
-  end
-  
-  function r.load_to_chest(sink_chest, count, item_id, task) 
-    if task.count_left == nil then 
-      task.count_left = count
-    end 
-    -- todo: select closest and fullest chest 
-    l.dbg(string.format("Loading %d x %d to chest", count, item_id))
-    for _, chest in ipairs(master.chests) do 
-      for i = 3, chest.slots_count do 
-        if chest.role == "storage" then 
-          local stack = chest.get_istack(i)
-          if item_id == stack[2] then 
-            local current_count = math.min(task.count_left, stack[1])
-            l.dbg(string.format("Transfering %d items from chest %d", current_count, chest.id))
-            if not chest.transfer_to(sink_chest, current_count, i, nil) then 
-              task.status = "error"
-              task.status_message = "Transfer failed."
-              return false 
-            end 
-            task.count_left = task.count_left - current_count
-            if task.count_left == 0 then 
-              l.info("Loading items into chest succeeded.")
-              return true 
-            end 
-          end 
+            return false
+          end
         end
       end
-    end 
-    
+    end
+    l.info("Loading items from chest succeeded.")
+    return true
+  end
+
+  function r.get_stored_item_counts(ids)
+    local ids_as_keys = {}
+    for _, id in ipairs(ids) do
+      ids_as_keys[id] = true
+    end
+    local result = {}
+    for _, chest in ipairs(master.chests) do
+      for i = 3, chest.slots_count do
+        local stack = chest.get_istack(i)
+        local item_id = stack[2]
+        if item_id ~= nil and ids_as_keys[item_id] then
+          result[item_id] = (result[item_id] or 0) + stack[1]
+        end
+      end
+    end
+    return result
+  end
+
+  function r.load_to_chest(sink_chest, count, item_id, task)
+    if task.count_left == nil then
+      task.count_left = count
+    end
+    -- todo: select closest and fullest chest
+    l.dbg(string.format("Loading %d x %d to chest", count, item_id))
+    for _, chest in ipairs(master.chests) do
+      for i = 3, chest.slots_count do
+        if chest.role == "storage" then
+          local stack = chest.get_istack(i)
+          if item_id == stack[2] then
+            local current_count = math.min(task.count_left, stack[1])
+            l.dbg(string.format("Transfering %d items from chest %d", current_count, chest.id))
+            if not chest.transfer_to(sink_chest, current_count, i, nil) then
+              task.status = "error"
+              task.status_message = "Transfer failed."
+              return false
+            end
+            task.count_left = task.count_left - current_count
+            if task.count_left == 0 then
+              l.info("Loading items into chest succeeded.")
+              return true
+            end
+          end
+        end
+      end
+    end
+
     l.error("Not enough items.")
-    return false 
-  
-  end 
-  
+    return false
+
+  end
+
   return r
-end 
-return module_r 
+end
+return module_r
--- craft2/master_main.lua
+++ craft2/master_main.lua
@@ -11,58 +11,58 @@ local r = {}
 
 local remote_databases = {}
 local remote_terminals = {}
-for _, host in ipairs(config.slaves) do 
+for _, host in ipairs(config.slaves) do
   table.insert(remote_databases, rpc.connect(hosts[host], nil, nil, "ping_once").item_database)
 end
-for _, host in ipairs(config.terminals) do 
+for _, host in ipairs(config.terminals) do
   local v = rpc.connect(hosts[host], nil, nil, "ping_once")
   table.insert(remote_databases, v.item_database)
   table.insert(remote_terminals, v.terminal)
 end
 
-r.log = {} 
-r.log.inspect = require("serialization").serialize 
-function r.log.message(obj) 
-  if obj.level == "warning" then  
+r.log = {}
+r.log.inspect = require("serialization").serialize
+function r.log.message(obj)
+  if obj.level == "warning" then
     obj.color = 0xffff00
-  elseif obj.level == "error" then 
+  elseif obj.level == "error" then
     obj.color = 0xff0000
-  elseif obj.level == "debug" then 
+  elseif obj.level == "debug" then
     obj.color = 0xb0b0b0
-    if not config.enable_debug_log then 
-      return 
-    end 
-  else 
+    if not config.enable_debug_log then
+      return
+    end
+  else
     obj.color = 0xffffff
-  end 
+  end
   gpu.setForeground(obj.color)
   print(obj.text)
   gpu.setForeground(0xffffff)
-  for _, t in ipairs(remote_terminals) do 
-    t.log_message(obj) 
-  end 
-end 
-function r.log.info(text) 
+  for _, t in ipairs(remote_terminals) do
+    t.log_message(obj)
+  end
+end
+function r.log.info(text)
   r.log.message({ level = "info", text = text })
-end 
-function r.log.warn(text) 
+end
+function r.log.warn(text)
   r.log.message({ level = "warning", text = text })
-end 
-function r.log.error(text) 
+end
+function r.log.error(text)
   r.log.message({ level = "error", text = text })
-end 
-function r.log.dbg(text) 
+end
+function r.log.dbg(text)
   r.log.message({ level = "debug", text = text })
-end 
+end
 local l = r.log
 
 
 function r.process_istack(istack)
-  if istack.unknown_stack then 
+  if istack.unknown_stack then
     local new_id = local_item_database.add(istack.unknown_stack)
-    for _, d in ipairs(remote_databases) do 
+    for _, d in ipairs(remote_databases) do
       d.set(new_id, istack.unknown_stack)
-    end 
+    end
     return { istack[1], new_id }
   else
     return istack
@@ -76,178 +76,183 @@ function r.run()
 
   l.dbg("Loading topology")
   local topology_data = fser.load(require("craft2/paths").topology)
-  if not topology_data then 
+  if not topology_data then
     l.warn("No topology file. Running rebuild...")
     topology_data = require("craft2/master_rebuild").run()
   end
   r.transposers = {}
   r.chests = {}
-  for i, d in ipairs(topology_data.transposers) do 
+  for i, d in ipairs(topology_data.transposers) do
     local interface
-    if d.modem_address then 
+    if d.modem_address then
       interface = rpc.connect(d.modem_address, nil, nil, "ping_once").transposers_interface
-    else 
-      -- local interface 
+    else
+      -- local interface
       interface = require("craft2/transposers_interface")
     end
     table.insert(r.transposers, wrap_transposer(interface, d.transposer_address))
   end
   r.chests_count = #(topology_data.chests)
-  for i, d in ipairs(topology_data.chests) do 
-    table.insert(r.chests, wrap_chest(i, d))    
+  for i, d in ipairs(topology_data.chests) do
+    table.insert(r.chests, wrap_chest(i, d))
   end
   l.dbg("Calculating final topology...")
-  local outcoming_chest = nil 
-  for _, chest in ipairs(r.chests) do 
+  local outcoming_chest = nil
+  for _, chest in ipairs(r.chests) do
     if chest.role == "output" then
-      if outcoming_chest == nil then 
-        outcoming_chest = chest 
-      else 
+      if outcoming_chest == nil then
+        outcoming_chest = chest
+      else
         l.warn("Multiple outcoming chests found.")
       end
-    end 
+    end
     chest.find_transposers_for_adjacent_chests()
   end
-  for _, chest in ipairs(r.chests) do 
+  for _, chest in ipairs(r.chests) do
     chest.find_paths_to_other_chests()
-  end  
-  
-  local tasks = {} 
+  end
+
+  local tasks = {}
   local previous_tasks_serialized = l.inspect(tasks)
   function send_tasks_if_changed()
     local new_tasks_serialized = l.inspect(tasks)
-    if new_tasks_serialized ~= previous_tasks_serialized then 
+    if new_tasks_serialized ~= previous_tasks_serialized then
       previous_tasks_serialized = new_tasks_serialized
       print("Tasks: "..new_tasks_serialized)
-      for _, t in ipairs(remote_terminals) do 
-        t.set_tasks(tasks) 
-      end 
-    end     
-  end 
-  
-  local master_is_quitting = false 
-  local rpc_interface = {} 
+      for _, t in ipairs(remote_terminals) do
+        t.set_tasks(tasks)
+      end
+    end
+  end
+
+  local master_is_quitting = false
+  local rpc_interface = {}
   local pending_commands = {}
-  function rpc_interface.command(cmd) 
-    if master_is_quitting then 
-      error("Master is offline.")
-    end 
-    table.insert(pending_commands, cmd)
-  end 
-  rpc.bind({ master = rpc_interface })
-  
   local item_storage = require("craft2/item_storage").create_storage()
-  
+  function rpc_interface.enqueue_command(cmd)
+    if master_is_quitting then
+      error("Master is offline.")
+    end
+    table.insert(pending_commands, cmd)
+  end
+  function rpc_interface.get_stored_item_counts(ids)
+    return item_storage.get_stored_item_counts(ids)
+  end
+  rpc.bind({ master = rpc_interface })
+
+
   function r.on_chest_failure()
     l.warn("Chest failure! I don't what to do with it yet!")
-  end 
-  
-  function process_task(task) 
-    if task.name == "incoming" then 
+  end
+
+  function process_task(task)
+    if task.name == "incoming" then
       l.dbg("Iterating over incoming chests")
-      for _, chest in ipairs(r.chests) do 
-        if chest.role == "incoming" then 
+      for _, chest in ipairs(r.chests) do
+        if chest.role == "incoming" then
           l.dbg("Processing incoming chest "..chest.id)
-          if not item_storage.load_all_from_chest(chest, task) then 
-            return false 
-          end 
-        end 
-      end 
-      l.dbg("Incoming task completed")
-      return true 
-    elseif task.name == "output" then 
-      if not outcoming_chest then 
-        l.error("Output task: no outcoming chest!")
-        return true 
-      end 
-      if type(task.item_id) ~= "number" then 
-        l.error("Output task: item_id is not a number!")
-        return true       
+          if not item_storage.load_all_from_chest(chest, task) then
+            return false
+          end
+        end
       end
-      if type(task.count) ~= "number" then 
+      l.dbg("Incoming task completed")
+      return true
+    elseif task.name == "output" then
+      if not outcoming_chest then
+        l.error("Output task: no outcoming chest!")
+        return true
+      end
+      if type(task.item_id) ~= "number" then
+        l.error("Output task: item_id is not a number!")
+        return true
+      end
+      if type(task.count) ~= "number" then
         l.error("Output task: count is not a number!")
-        return true             
-      end 
-      if task.count < 1 then 
+        return true
+      end
+      if task.count < 1 then
         l.error("Output task: count is not positive enough.")
-        return true             
-      end 
+        return true
+      end
       return item_storage.load_to_chest(outcoming_chest, task.count, task.item_id, task)
     else
       l.error("Unknown task")
       return true
-    end 
-  end 
-  
-  local next_task_id = 1 
+    end
+  end
+
+  local next_task_id = 1
   local tick_interval = 1
   local tick_interval_after_error = 10
-  
+
   function tick()
     local is_ok, err = xpcall(function()
-      while #pending_commands > 0 do 
+      while #pending_commands > 0 do
         local cmd = pending_commands[1]
         table.remove(pending_commands, 1)
-        if cmd.action == "add_task" then 
+        if cmd.action == "add_task" then
           cmd.task.id = next_task_id
           next_task_id = next_task_id + 1
           table.insert(tasks, cmd.task)
           l.dbg("Task added: "..l.inspect(cmd.task))
           send_tasks_if_changed()
-        elseif cmd.action == "remove_task" then 
-          local ok = false 
-          for i, task in ipairs(tasks) do 
-            if task.id == cmd.task_id then 
+        elseif cmd.action == "remove_task" then
+          local ok = false
+          for i, task in ipairs(tasks) do
+            if task.id == cmd.task_id then
               table.remove(tasks, i)
               l.dbg("Task is removed by user.")
               ok = true
               break
-            end 
-          end 
-          if not ok then 
+            end
+          end
+          if not ok then
             l.error("Cannot remove task: task not found.")
-          end 
+          end
           send_tasks_if_changed()
-        elseif cmd.action == "quit" then 
-          master_is_quitting = true       
+        elseif cmd.action == "quit" then
+          master_is_quitting = true
           l.info("Master is now offline.")
-          return 
-        elseif cmd.action == "throw_error" then 
+          return
+        elseif cmd.action == "throw_error" then
           error("No, it's YOUR fault.")
-        else 
+        else
           l.error("Invalid action.")
-        end 
-      end 
+        end
+      end
 
       table.sort(tasks, function(a,b) return (a.priority or 0) > (b.priority or 0) end)
-      for i, task in ipairs(tasks) do 
+      local tasks_left = {}
+      for i, task in ipairs(tasks) do
         l.dbg("Running task: "..l.inspect(task))
-        if process_task(task) then 
-          table.remove(tasks, i)
+        if process_task(task) then
           l.dbg("Task is completed.")
-          break
-        end 
-      end 
+        else
+          table.insert(tasks_left, task)
+        end
+      end
+      tasks = tasks_left
       send_tasks_if_changed()
-      for _, chest in ipairs(r.chests) do 
+      for _, chest in ipairs(r.chests) do
         chest.save_cache()
-      end  
+      end
       if not master_is_quitting then
         event.timer(tick_interval, tick)
-      end 
-    end, function(err) 
+      end
+    end, function(err)
       return { message = tostring(err), traceback = debug.traceback() }
     end)
-    if not is_ok then     
+    if not is_ok then
       l.error("Error: "..err.message)
       print(err.traceback)
       if not master_is_quitting then
         event.timer(tick_interval_after_error, tick)
-      end 
-    end 
-  end 
+      end
+    end
+  end
   event.timer(tick_interval, tick)
   l.info("Master is now live.")
-  
-end 
+
+end
 return r
