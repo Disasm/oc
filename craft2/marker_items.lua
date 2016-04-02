4952c3d721a52d6c27bf577693393c9f11218a11
Riateche <ri@idzaaus.org>
2016-04-02 23:44:29 +0300

    new markers

--- craft2/marker_items.lua
+++ craft2/marker_items.lua
@@ -2,13 +2,29 @@
 local r = {};
 
 function r.stack_to_chest_role(stack)
-  if stack == nil then return "storage" end 
-  if stack.label == "Stick" then 
+  if stack == nil then return "storage" end
+  if stack.label == "Stick" then
     if stack.size == 1 then
       return "incoming"
-    elseif stack.size == 2 then 
+    elseif stack.size == 2 then
       return "output"
     end
+  elseif stack.label == "Crafting Table" then
+    return "craft"
+  elseif stack.label == "HV Cable" then
+    return "Extruder"
+  elseif stack.label == "Iron Item Casing" then
+    return "Roller"
+  elseif stack.label == "Dense Iron Plate" then
+    return "Compressor"
+  elseif stack.label == "Iron Coin" then
+    return "Furnace"
+  elseif stack.label == "Rubber" then
+    return "Extractor"
+  elseif stack.label == "Iron Dust" then
+    return "Macerator"
+  else
+    print(string.format("Unknown marker: '%s'", stack.label))
   end
   return "storage"
 end
