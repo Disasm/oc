8b5af9d32b1a19af27e9d838bf4b3d78866b7d4e
Riateche <ri@idzaaus.org>
2016-04-03 03:05:00 +0300

    ndns bug fix

--- ndns.lua
+++ ndns.lua
@@ -3,7 +3,7 @@ local filesystem = require("filesystem")
 local event = require("event")
 local fser = require("libs/file_serialization")
 
-local modem = component.modem 
+local modem = component.modem
 local hosts_filename = "/lib/hosts.lua"
 
 local the_port = 10101
@@ -14,29 +14,29 @@ function get_hostname()
   local s = f:read(math.huge)
   f:close()
   return s
-end 
-  
+end
+
 local hostname = get_hostname()
-local hosts = fser.load(hosts_filename)
-if not hosts then 
+local hosts = require(hosts_filename)
+if not hosts then
   hosts = {}
   fser.save(hosts_filename, hosts, true)
-end 
+end
 print("My hostname: "..hostname)
-for k, v in pairs(hosts) do 
-  print("Known hostname: "..k)  
+for k, v in pairs(hosts) do
+  print("Known hostname: "..k)
 end
 
 
-function on_message(_, _, remote_addr, port, _, payload) 
-  if the_port == port then 
-    if not hosts[payload] then 
+function on_message(_, _, remote_addr, port, _, payload)
+  if the_port == port then
+    if not hosts[payload] then
       print("New host added: "..payload)
       hosts[payload] = remote_addr
       fser.save(hosts_filename, hosts, true)
     end
   end
-end 
+end
 
 function on_timer()
   modem.broadcast(the_port, hostname)
